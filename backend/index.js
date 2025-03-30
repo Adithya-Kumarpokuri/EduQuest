8749b1283813bfd5de20a830c1eb6a9bc96e97c5
Adithya Kumar <adithyakumarpokuri978@gmail.com>
2025-03-31 00:24:22 +0530

    Saving all changes before pulling

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { db } = require('./db/db');
+
+const app = express();
+const Routes = require("./routes/route.js");
+
+app.use(express.json());
+app.use(cors());
+require('dotenv').config();
+
+
+const PORT = process.env.PORT;
+app.use("/",Routes)
+const server = () => {
+  db()
+  app.listen(PORT, () => {
+      console.log('listening to port:', PORT)
+  })
+}
+server()
